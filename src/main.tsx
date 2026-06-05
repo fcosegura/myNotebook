9966517418fba3485dd53ec19ce2db704bd487f0
fcosegura <135116062+fcosegura@users.noreply.github.com>
2026-06-05 16:28:29 +0200

    Load light theme override stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.tsx'
+import './light-theme.css'
 
 registerSW({ immediate: true })
 
